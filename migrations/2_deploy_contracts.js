5962e1b3d3d109f6f70d97cb24456b214d2bd589
Jinhwan <46812766+Midnight8@users.noreply.github.com>
2021-02-06 02:46:12 +0900

    스마트 컨트랙트 추가
    클레이튼 기반 스마트 컨트랙트 추가 및
    클레이튼 환경에 맞게 js 파일 수정

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,25 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const AdditionGame = artifacts.require('./AdditionGame.sol')
+const fs = require('fs')
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-};
+module.exports = function (deployer) {
+  deployer.deploy(AdditionGame)
+    .then(() => {
+      if (AdditionGame._json) {
+        fs.writeFile(
+          'deployedABI',
+          JSON.stringify(AdditionGame._json.abi),
+          (err) => {
+            if (err) throw err
+            console.log("파일에 ABI 입력 성공");
+          })
+      }
+
+      fs.writeFile(
+        'deployedAddress',
+        AdditionGame.address,
+        (err) => {
+          if (err) throw err
+          console.log("파일에 주소 입력 성공");
+        })
+    })
+}
