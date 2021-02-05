8243f932756799f063058041e66178edbdef2b61
tmdals8030 <48111893+tmdals8030@users.noreply.github.com>
2021-02-05 16:13:21 +0900

    오류 수정

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Login, About, User } from './pages';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Login</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/user">User</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route exact path='/' component={Login}/>
+          <Route path='/about' component={About}/>
+          <Route path='/user' component={User}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Login, About, User } from './pages';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+//import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Login</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/user">User</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Route exact path='/' component={Login}/>
-          <Route path='/about' component={About}/>
-          <Route path='/user' component={User}/>
-        </div>
-      </Router>
-    );
-  }
-}
-
-export default App;
+ReactDOM.render(<Root />, document.getElementById('root'));
+//registerServiceWorker();
